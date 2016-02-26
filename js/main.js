3d7522499c0751ca61934c898fa26ce3e8b1effe
Taylor Sargent <sarg0049@algonquinlive.com>
2016-02-26 10:36:06 -0500

    added in another loop

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ var newLetter = prompt('Enter a letter:');
 
 var i = 0;
 
-for (i = 0; i < 10; i + 1) {
-  document.write(newLetter + '<br>');
+var j = 0;
+
+for (i = 0; i < 10; i++) {
+  for (j = 0; j < i; j++) {
+    document.write(newLetter);
+  }
+  document.write('<br>');
 }
